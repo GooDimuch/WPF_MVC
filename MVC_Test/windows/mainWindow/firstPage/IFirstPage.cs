85c9cb20acf406156a88a349fb5f01209a34dc3e
GooDimuch <GooDimuch@gmail.com>
2019-12-11 20:15:55 +0200

    forgot exstend IBasePage for IFirstPage

--- MVC_Test/windows/mainWindow/firstPage/IFirstPage.cs
+++ MVC_Test/windows/mainWindow/firstPage/IFirstPage.cs
@@ -1,5 +1,7 @@
-﻿namespace MVC_Test.windows.mainWindow.firstPage {
-	internal interface IFirstPage {
+﻿using MVC_Test.windows.basePage;
+
+namespace MVC_Test.windows.mainWindow.firstPage {
+	internal interface IFirstPage : IBasePage {
 		void FirstPageMethod();
 	}
 }
