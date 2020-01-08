16f8ef0f8b97f64ca8aba87badded8dc101ce15c
GooDimuch <goodimuch@gmail.com>
2020-01-08 19:29:44 +0200

    create custom dropbox with categories

--- MVC_Test/customViews/dropBoxWithCategories/DropBoxWithCategories.xaml.cs
+++ MVC_Test/customViews/dropBoxWithCategories/DropBoxWithCategories.xaml.cs
@@ -37,11 +37,18 @@ namespace MVC_Test.customViews.dropBoxWithCategories {
 		}
 
 		private BaseDropBoxModel Model;
+		private BaseCategory SelectedCategory;
+		private bool isChanged;
 
 		public DropBoxWithCategories() {
 			InitializeComponent();
 			Model = CreateDropBoxModel();
 			DropBox.ItemsSource = new List<DropBoxItem>(Model.CategoryList);
+		}
+
+		private void DropBoxWithCategories_OnLoaded(object sender, RoutedEventArgs e) {
+			DropBox.DropDownOpened += DropDownOpened;
+			DropBox.DropDownClosed += DropDownClosed;
 		}
 
 		private static BaseDropBoxModel CreateDropBoxModel() {
@@ -60,23 +67,50 @@ namespace MVC_Test.customViews.dropBoxWithCategories {
 			Console.WriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod().Name}");
 		}
 
+		private void DropDownOpened(object sender, EventArgs e) { }
+
+		private void DropDownClosed(object sender, EventArgs e) {
+			if (isChanged) {
+				isChanged = false;
+				DropBox.IsDropDownOpen = true;
+			} else { }
+		}
+
+		/// <summary>
+		/// Изначально берем вписок всех категорий. Потом определяем тип нажатого объекта. Если это категория, то проверяем
+		/// открыта ли она сейчас, если открыта, то закрываем, а если закрыта, то берем у нее список всех дочерних объектов
+		/// и вставляем по (индексу категории + 1)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void EventSetter_OnHandler(object sender, MouseButtonEventArgs e) {
+			if (isChanged) { return; } else { isChanged = true; }
 			Console.WriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod().Name}");
-			if (!(sender is DropBoxItem item)) { return; }
-			List<DropBoxItem> itemList = new List<DropBoxItem>();
+			if (!(sender is ComboBoxItem cbItem) || !cbItem.Content.GetType().IsSubclassOf(typeof(DropBoxItem))) { return; }
+			var item = cbItem.Content as DropBoxItem ?? throw new Exception("can't cast item to DropBoxItem");
+			//todo взять список категорий
+			var itemList = Model.CategoryList.Cast<DropBoxItem>().ToList();
+			BaseCategory category;
 			switch (item.Type) {
 				case ItemType.Category:
-					//todo взять список категорий
-					itemList = Model.CategoryList.Cast<DropBoxItem>().ToList();
+					category = item as BaseCategory ?? throw new Exception("can't cast item to BaseCategory");
+					if (SelectedCategory?.Equals(category) ?? false) {
+						SelectedCategory = null;
+						break;
+					}
 					//todo найти индекс выбранной категории
-
+					var selectedIndex = itemList.IndexOf(category);
 					//todo вставить по индексу все элементы выбранной категории
+					itemList.InsertRange(selectedIndex + 1, category.ItemList);
+					SelectedCategory = category;
 					break;
 				case ItemType.Item:
 					//todo найти категорию выброного элемента
+					category = Model.CategoryList.ToList().Find(baseCategory => baseCategory.ItemList.Contains(item));
 					//todo указать в поле SelectedItem выбранный элемент
-					//todo взять список категорий
-					itemList = Model.CategoryList.Cast<DropBoxItem>().ToList();
+					category.SelectedItem = item as BaseCategoryItem ?? throw new Exception("can't cast item to BaseCategory");
+					//todo очистить 
+					SelectedCategory = null;
 					break;
 				default: throw new ArgumentOutOfRangeException();
 			}
@@ -110,24 +144,32 @@ namespace MVC_Test.customViews.dropBoxWithCategories {
 	}
 
 	public class BaseCategory : DropBoxItem {
-		public string Title;
 		public List<BaseCategoryItem> ItemList;
-		public BaseCategoryItem SelectedItem;
+		public string Name;
+		private BaseCategoryItem _selectedItem;
 
-		public BaseCategory(string title, List<BaseCategoryItem> itemList) : base(DropBoxWithCategories.ItemType.Category,
-																																							title) {
-			Title = title;
+		public BaseCategoryItem SelectedItem {
+			get => _selectedItem;
+			set {
+				_selectedItem = value;
+				Title = $"{Name}: [{SelectedItem.Name}]";
+			}
+		}
+
+		public BaseCategory(string name, List<BaseCategoryItem> itemList) : base(DropBoxWithCategories.ItemType.Category,
+																																						name) {
+			Name = name;
 			ItemList = itemList;
 			if (itemList.Count > 0) { SelectedItem = itemList[0]; }
 		}
 	}
 
 	public class BaseCategoryItem : DropBoxItem {
-		public string Title;
 		public string Key;
+		public string Name;
 
-		public BaseCategoryItem(string title, string key) : base(DropBoxWithCategories.ItemType.Item, title) {
-			Title = title;
+		public BaseCategoryItem(string name, string key) : base(DropBoxWithCategories.ItemType.Item, name) {
+			Name = name;
 			Key = key;
 		}
 	}
